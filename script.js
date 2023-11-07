e0c5aff70f7d30e15f54b647faa1825bb86b46e3
Soumik-0406 <124161684+Soumik-0406@users.noreply.github.com>
2023-11-07 10:50:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let input = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
+let string = "";
+// var a = string 1;
+// var b = string 2;
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        // else if (e.target.innerHTML == '%') {
+        //     string = (a/100)*b;
+        //     input.value = string;
+        // }
+        else {
+                string += e.target.innerHTML;
+                input.value = string;
+            }
+
+        })
+})
